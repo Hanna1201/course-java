4ee8a7b72583f33efdc5ab6846ffd22177bb96bb
Hanna Kavaliova <a.kovaleva@21vek.by>
2022-09-08 16:20:16 +0300

    feat: add task02 for lesson2

---
+++ src/lessons/two/Task02.java
@@ -0,0 +1,12 @@
+package lessons.two;
+
+public class Task02 {
+    public static void main(String[] args) {
+        int w = 7;
+        int h = 5;
+        int perimeter = (w+h)*2;
+        int square = w*h;
+        System.out.println("Периметр прямоугольника равен " + perimeter);
+        System.out.println("Площадь прямоугольника равна " + square);
+    }
+}
