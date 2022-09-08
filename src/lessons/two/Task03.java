f89fa94da947e0fd2ca06ce6fcd796588be7e63f
Hanna Kavaliova <a.kovaleva@21vek.by>
2022-09-08 19:06:52 +0300

    feat: add task03 for lesson2 (area and circumference of a circle)

---
+++ src/lessons/two/Task03.java
@@ -0,0 +1,12 @@
+package lessons.two;
+
+public class Task03 {
+    public static void main(String[] args) {
+        double r = 7;
+        final double PI = 3.14;
+        double p = 2*PI*r;
+        double s = PI*r*r;
+        System.out.println("Длина окружности круга равна " + p);
+        System.out.println("Площадь круга равна " + s);
+    }
+}
